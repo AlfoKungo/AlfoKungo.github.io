d18ce3e7fb79df66bdfe287f27bf6ef4f7f5db7b
Gal Tamam <gal.tamam@armoz.io>
2023-08-05 23:27:17 +0300

    add counter and remove when counter at 4

--- src/questions/Clicker.js
+++ src/questions/Clicker.js
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import ClickerButton from "./ClickerButton";
 
 export default function Clicker(props) {
-  let terms = props.terms;
+  const LIMIT = 5;
+  const [terms, setTerms] = useState(props.terms);
+  const [levels, setLevels] = useState(new Array(terms.length).fill(0));
+  const [l_index, setLIndex] = useState(0);
 
   let cards = [];
-  // for (let i = 0; i < 50 ; i++)
   for (let i = 0; i < terms.length; i++) {
     cards.push(terms[i]);
   }
+  const increaseLevel = (index) => {
+    if (levels[index] < LIMIT) {
+      const newLevels = [...levels];
+      newLevels[index] += 1;
+      setLevels(newLevels);
+    } else {
+      deleteItem(index);
+    }
+  };
 
+  const deleteItem = (index) => {
+    const newLevels = levels.filter((_, i) => i !== index);
+    setLevels(newLevels);
+    const newTerms = terms.filter((_, i) => i !== index);
+    setTerms(newTerms);
+  };
+  const reduceLevel = (index) => {
+    const newLevels = [...levels];
+    newLevels[index] -= 1;
+    setLevels(newLevels);
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "=" && levels[l_index] < LIMIT) increaseLevel(l_index);
+    if (e.key === "-" && levels[l_index] > 0) reduceLevel(l_index);
+  };
   return (
-    <div className="click-card">
+    <div className="click-card" onKeyDown={onKeyPress} tabIndex="0">
       <ul>
         {cards.map((card, index) => {
           return (
@@ -19,6 +46,12 @@ export default function Clicker(props) {
               index={index}
               in_line_delimeter=";"
               key={card}
+              level={levels[index]}
+              onClick={(e) => {
+                setLIndex(index);
+                if (e.metaKey) increaseLevel(index);
+                if (e.altKey && levels[index] > 0) reduceLevel(l_index);
+              }}
             />
           );
         })}
--- src/questions/ClickerButton.js
+++ src/questions/ClickerButton.js
@@ -8,32 +8,44 @@ function flip_back(setFlip, id) {
   document.querySelector("#" + id).classList.remove("clicker");
 }
 
-function know_it(id) {
-  document.getElementById(id).style.backgroundColor = "green";
-}
+// function know_it(id) {
+//   document.getElementById(id).style.backgroundColor = "green";
+// }
 
 export default function ClickerButton(props) {
   let card = props.card;
   let index = props.index;
   let id = "que" + index;
+  let level = props.level;
+  let onClick = props.onClick;
+  // let level = 0;
   in_line_delimeter = props.in_line_delimeter;
 
   const [flip, setFlip] = useState(false);
+  // const [level, setLevel] = useState(_level);
 
   let [que, ans] = card.split(in_line_delimeter);
   if (switch_sides) [que, ans] = [ans, que];
-
+  const shadesOfGrey = [
+    "#000000", // Black
+    "#606060", // Dark grey
+    "#808080", // Grey
+    "#B0B0B0", // Light grey
+    "#D3D3D3", // Lighter grey
+    "#EEEEEE", // Lightest grey
+  ];
   // document.getElementById(que).addEventListener("contextmenu", e => e.preventDefault());
   return (
     <div
       className="click-button"
       id={id}
+      onClick={onClick}
+      style={{ color: shadesOfGrey[level] }}
+
       //   onContextMenu={(e)=> e.preventDefault()}
     >
       <li
         onClick={(e) => {
-          console.log(index);
-
           let ele = document.getElementById(id);
           setFlip(true);
           document.querySelector("#" + id).classList.add("clicker");
@@ -43,11 +55,12 @@ export default function ClickerButton(props) {
         }}
         id={id + "li"}
         onContextMenu={(e) => {
-          know_it(id);
+          // know_it(id);
           e.preventDefault();
         }}
         className=""
       >
+        <div className="clicker-level">{level}</div>
         {flip ? que : ans}
       </li>
     </div>
